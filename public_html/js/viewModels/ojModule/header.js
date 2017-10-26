332d6a4f4b6bf1988ec58c2a7270171493c99561
jsbimra <>
2017-10-26 11:16:06 +0530

    punk api implemented with search functionality and detail view in oracle jet modile

--- public_html/js/viewModels/ojModule/header.js
+++ public_html/js/viewModels/ojModule/header.js
@@ -13,59 +13,62 @@ define(['ojs/ojcore', 'knockout', 'ojs/ojoffcanvas', 'ojs/ojnavigationlist', 'oj
      * The view model for the main content view template
      */
     function headerContentViewModel() {
-      var self = this;
-      var rootVM = ko.dataFor(document.getElementById('pageContent'));
-      
-      self.cartItemsCount = ko.observable(rootVM.cartCount());
-      
-      self.cartItemsCount = ko.computed(function(){
-         return rootVM.cartCount(); 
-      });
-     
-      // Media queries for repsonsive layouts
-      var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
-      self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
-      var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
-      self.mdScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+        var self = this;
+        var rootVM = ko.dataFor(document.getElementById('pageContent'));
 
-      // Drawer
-      // Close offcanvas on medium and larger screens
-      self.mdScreen.subscribe(function() {oj.OffcanvasUtils.close(self.drawerParams);});
-      self.drawerParams = {
-        displayMode: 'push',
-        selector: '#navDrawer',
-        content: '#pageContent'
-      };
-      // Called by navigation drawer toggle button and after selection of nav drawer item
-      self.toggleDrawer = function() {
-        return oj.OffcanvasUtils.toggle(self.drawerParams);
-      };
-      
+        self.cartItemsCount = ko.observable(rootVM.cartCount());
+
+        self.cartItemsCount = ko.computed(function () {
+            return rootVM.cartCount();
+        });
+
+        // Media queries for repsonsive layouts
+        var smQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.SM_ONLY);
+        self.smScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(smQuery);
+        var mdQuery = oj.ResponsiveUtils.getFrameworkQuery(oj.ResponsiveUtils.FRAMEWORK_QUERY_KEY.MD_UP);
+        self.mdScreen = oj.ResponsiveKnockoutUtils.createMediaQueryObservable(mdQuery);
+
+        // Drawer
+        // Close offcanvas on medium and larger screens
+        self.mdScreen.subscribe(function () {
+            oj.OffcanvasUtils.close(self.drawerParams);
+        });
+        self.drawerParams = {
+            displayMode: 'push',
+            selector: '#navDrawer',
+            content: '#pageContent'
+        };
+        // Called by navigation drawer toggle button and after selection of nav drawer item
+        self.toggleDrawer = function () {
+            return oj.OffcanvasUtils.toggle(self.drawerParams);
+        };
+
 //      self.goToDefaultPage = function(){
 //            console.log(self);
 //            self.router.go('/');
 //      };
-      
-      // Add a close listener so we can move focus back to the toggle button when the drawer closes
-      $("#navDrawer").on("ojclose", function() { $('#drawerToggleButton').focus(); });
 
-      // Header
-      // Application Name used in Branding Area
-      self.appName = ko.observable("JET POC");
-      // User Info used in Global Navigation area
-      self.userLogin = ko.observable("john.hancock@oracle.com");
-      
-      // Navigation setup
-      var navData = [
-      {name: 'Product', id: 'product',
-       iconClass: 'oj-navigationlist-item-icon'},
-      {name: 'About', id: 'about',
-       iconClass: 'oj-navigationlist-item-icon'}
-      ];
-      self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
-      
-      
+        // Add a close listener so we can move focus back to the toggle button when the drawer closes
+        $("#navDrawer").on("ojclose", function () {
+            $('#drawerToggleButton').focus();
+        });
+
+        // Header
+        // Application Name used in Branding Area
+        self.appName = ko.observable("JET POC");
+        // User Info used in Global Navigation area
+        self.userLogin = ko.observable("john.hancock@oracle.com");
+
+        // Navigation setup
+        var navData = [
+            {name: 'Product', id: 'product', iconClass: 'oj-navigationlist-item-icon'},
+            {name: 'About', id: 'about', iconClass: 'oj-navigationlist-item-icon'},
+            {name: 'Punk beer', id: 'punk', iconClass: 'oj-navigationlist-item-icon'}
+        ];
+        self.navDataSource = new oj.ArrayTableDataSource(navData, {idAttribute: 'id'});
+
+
     }
-    
+
     return headerContentViewModel;
 });
---
+++ public_html/js/viewModels/punk.js
@@ -0,0 +1,121 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * giphy module
+ */
+define(['ojs/ojcore', 'knockout', 'jquery', 'underscore', 'services/punkService',
+    'ojs/ojmasonrylayout', 'ojs/ojprogress', 'ojs/ojinputtext', 'ojs/ojdialog'],
+        function (oj, ko, $, _, punkService) {
+            /**
+             * The view model for the main content view template
+             */
+            function punkContentViewModel() {
+
+                var vm = this;
+                var bArray = null;
+                
+                // Service invoke               
+                var pServiceInstance = new punkService();
+                
+                vm.promiseFailed = ko.observable(false);
+                vm.loaderFlag = ko.observable(true);
+                
+                vm.beers = ko.observableArray();
+                vm.progressValue = ko.observable(0);
+                vm.searchValue = ko.observable('');
+                vm.handleOKClose = handleOKClose;
+                vm.openBeerDetailView = openBeerDetailView;
+                vm.dialogDetails = ko.observable("");
+
+
+                var loaderInterval = setInterval(function () {
+                    vm.progressValue(vm.progressValue() + 5);
+                }, 30);
+
+                
+        
+                //service request to fetch the punk data                
+                pServiceInstance.fetchBeers.then(function (data) {
+                    console.log('success');
+                    clearInterval(loaderInterval);
+                    vm.progressValue(100);
+                    vm.loaderFlag(false);
+                    vm.promiseFailed(false);
+
+
+                    if (data) {
+                        
+                        bArray = data;
+                        
+                        vm.beers(bArray);
+
+                        //console.log(bArray);
+                    }
+
+                }).fail(function(err){
+                    
+                    console.log(err);
+                    
+                    vm.loaderFlag(false);
+                    vm.promiseFailed(true);
+
+                    if (err.responseJSON) {
+
+                        var respVal = err.responseJSON;                        
+                        var errorText = 'API ERROR : Status Code ' + respVal.statusCode + ' >> ' + respVal.error + ' >> ' + respVal.message;
+                        
+                        //Bind the value to view
+                        $('#promiseFailedText').text(errorText);
+                    }   
+                });
+
+                //define search method
+                function performSearch(value) {
+                    
+                    //console.clear();
+
+                    //console.log('bArray data', bArray);
+                    //console.log('current val', value);
+                        
+                    if(value.length > 0){
+                        
+                        //Empty the current beers list and fill it with value found in the root data
+                        vm.beers([]);                        
+                        
+                        //console.log('if length greater than 2');
+                        //console.log('bArray value now ',bArray);
+                        
+                        for (var x in bArray) {
+
+                            if (bArray[x].name.toLowerCase().indexOf(value.toLowerCase()) >= 0) {
+                                vm.beers.push(bArray[x]);
+                            }
+                        }
+                        
+                    }else{
+                        //console.log('inside else ', bArray);
+                        vm.beers(bArray);
+                    }
+                }
+
+                //we subscribe on searchValue whenenver it changed and update our beers list again 
+                vm.searchValue.subscribe(performSearch);
+                
+                //Detail view definiation to show detail                
+                function openBeerDetailView(curItem){
+                    console.log(curItem);
+                    vm.dialogDetails(curItem);
+                    document.querySelector('#punkDetailDialog').open();
+                }
+                
+                function handleOKClose() {
+                    document.querySelector("#punkDetailDialog").close();
+                };
+            }
+
+            return punkContentViewModel;
+        });
